efb803fe1355c9a3df6f88e19044d316178029d8
Tracy Boateng <kusiwaahboateng1997@gmail.com>
2024-02-12 05:54:47 -0800

    Working on appy nav bar

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// alert('We are Beta!!!');
+console.log('We are Beta');
+var firstName = 'Tracy';
+var age = 2;
+let lastName = 'Boateng';
+const fixativeTemperature = 45;
+
+console.log(firstName)
+console.log(age)
+console.log(lastName)
+console.log(fixativeTemperature)
+fixativeTemperature = 75;
